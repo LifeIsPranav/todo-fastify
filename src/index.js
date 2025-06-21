00753cb1a42a1c54742de2b12593ae1bd3f83282
Pranav <lifeispranav@gmail.com>
2025-06-21 20:48:56 +0530

    setup of fastify done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')({ logger: true })  // Root Application Instance
+
+fastify.addHook('onReady', function listener(done) {
+  console.log('Server is Ready to Take Request')
+  done()
+})
+
+fastify.addHook('onClose', function listener(done) {
+  console.log('Server is Stopping') 
+})
+
+const PORT = 8080
+async function start() {
+  try {
+    await fastify.listen({ port: PORT })
+    console.log(`Server is Up on PORT: ${PORT}`)
+
+    await setTimeout(() => {
+      fastify.close()
+    }, 5000)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
